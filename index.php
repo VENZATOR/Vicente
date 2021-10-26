c090ddc9e6a25d597b84de813d19d9ba70ee96f2
VENZATOR <vicente.gior14@gmail.com>
2021-10-26 20:30:26 +0200

    hola

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php 
-$car = "b";
-$cad = "la vida es muy bonita y agradable";
-echo("numero de -$car- dentro de -$cad-: ");
-echo(substr_count($cad,$car));
-?>
-<br>
-<?php
-echo("numero de espacio dentro de -$cad-: ");
-echo(substr_count($cad," "));
+HEADER  ("location:/index.html");
 ?> 
